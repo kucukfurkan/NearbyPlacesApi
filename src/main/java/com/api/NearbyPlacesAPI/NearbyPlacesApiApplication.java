2c0dbadf8653b9a00e7400bff2fbb1d2aa0743f8
Tasarim Lumos <furkannkck@outlook.com>
2024-01-05 16:43:33 +0300

    Initial commit

---
+++ src/main/java/com/api/NearbyPlacesAPI/NearbyPlacesApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.NearbyPlacesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyPlacesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyPlacesApiApplication.class, args);
+	}
+
+}
